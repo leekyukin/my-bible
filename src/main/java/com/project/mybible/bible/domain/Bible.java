3495aa79933156bbb69957d54df0e3d3e8d6a624
leekyukin <leekujin14@gmail.com>
2022-06-29 23:55:50 +0900

    ADD :: ENTITY

---
+++ src/main/java/com/project/mybible/bible/domain/Bible.java
@@ -0,0 +1,28 @@
+package com.project.mybible.bible.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "bible2")
+public class Bible {
+
+    @Id
+    private Long idx;
+
+    private int cate; // 구/신약 ( 1, 2 )
+    private int book; // 책 (숫자 1 ~ 66)
+    private int chapter; // 장
+    private int paragraph; // 절
+    private String sentence; // 문장
+    private char testament; // 구/신약 ('구', '신')
+
+    @Column(name = "long_label")
+    private String longLabel; // 책 ("창세기", "출에굽기")
+
+    @Column(name = "short_label")
+    private String shortLabel; // 책 ("창", "출")
+}
+

905eece41f3c305433bc77717840689fb7e5c370
leekyukin <leekujin14@gmail.com>
2022-06-29 14:45:57 +0900

    SET :: project & gitignore

---
+++ src/main/java/com/project/mybible/MyBibleApplication.java
@@ -0,0 +1,13 @@
+package com.project.mybible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBibleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBibleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/mybible/MyBibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mybible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBibleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

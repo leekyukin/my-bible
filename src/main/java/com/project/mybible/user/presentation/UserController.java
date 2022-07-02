470f3b33c459c665fd53cc31b5862fa597a691d3
leekyukin <leekujin14@gmail.com>
2022-07-02 17:55:56 +0900

    ADD :: Join api

---
+++ src/main/java/com/project/mybible/user/presentation/UserController.java
@@ -0,0 +1,24 @@
+package com.project.mybible.user.presentation;
+
+import com.project.mybible.user.presentation.dto.request.CreateUserRequestDto;
+import com.project.mybible.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public void join(
+            @RequestBody CreateUserRequestDto request
+    ) {
+        userService.join(request);
+    }
+}
